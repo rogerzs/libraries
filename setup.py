6d50cdae402235c0b961b9572db03e1267aca7a8
rogerzs <56163805+rogerzs@users.noreply.github.com>
2021-03-18 16:08:49 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name='transform_parameters'
+  version='0.0.1'
+  author='Rogério Ferreira'
+  author_email='rogeroficial1506@gmail.com'
+  description='Small transformation package to specific project'
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url="https://github.com/rogerzs/libraries",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Porgramming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  
+  ],
+  python_requires='>=3.6',
+)
