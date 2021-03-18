5398ebe6dbb241d68db3eabb9348400725c0d4c4
Rogério Ferreira dos Santos <ds_rogerio.santos@bancointer.com.br>
2021-03-18 16:25:33 -0300

    teste

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
   long_description = fh.read()
 
 setuptools.setup(
-  name='transform_parameters'
-  version='0.0.1'
-  author='Rogério Ferreira'
-  author_email='rogeroficial1506@gmail.com'
-  description='Small transformation package to specific project'
+  name='transform_parameters',
+  version='0.0.1',
+  author='Rogério Ferreira',
+  author_email='rogeroficial1506@gmail.com',
+  description='Small transformation package to specific project',
   long_description=long_description,
   long_description_content_type='text/markdown',
   url="https://github.com/rogerzs/libraries",
